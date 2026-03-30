e85d535246e9ac51c2abd152e87503949e9fe865
Mufacoderz <mf9555759@gmail.com>
2026-03-30 17:44:53 +0800

    migrate+sedd

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,9 +18,12 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::create([
+            'nama' => 'Atmin',
+            'email' => 'atmin@gmail.com',
+            'jabatan' => 'Admin',
+            'password' => Hash::make('12345678'),
+            'is_tugas' => false,
         ]);
     }
 }
